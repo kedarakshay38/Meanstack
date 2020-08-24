44b08856992dfd95cecd58260b1c2b9605ae87a9
kedarakshay38 <65217534+kedarakshay38@users.noreply.github.com>
2020-08-24 09:32:50 +0530

    Add files via upload

---
+++ create class and its function1/j1.js
@@ -0,0 +1,14 @@
+class Mainclass {
+    static main() {
+        console.log("Hello World Like java");
+    }
+
+    static add(a, b) {
+        let c = a + b;
+        console.log(c);
+    }
+}
+
+
+Mainclass.add(10, 20);
+Mainclass.main();
